d70e861320297bf83431c73a480cd5f1c78e906d
zhangcn <15236674712@163.com>
2020-02-25 10:25:02 +0800

    第三次  添加了一个测试类

---
+++ src/test/java/com/baizhi/GitTests.java
@@ -0,0 +1,21 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("这是第一次修改");
+        System.out.println("这是第二次修改");
+        int a = 10;
+        int b = 20;
+        System.out.println("这是第二次修改");
+    }
+
+}
